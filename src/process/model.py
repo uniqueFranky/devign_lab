fix acc calculation

src/process/model.py
@@ -66,7 +66,7 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.ggc = GatedGraphConv(**gated_graph_conv_args).to(device) 
         self.emb_size=emb_size
-        self.readout = Readout(max_nodes)
+        self.readout = Readout(max_nodes).to(device)
         
 
     def forward(self, data):

src/process/step.py
@@ -3,7 +3,8 @@ from ..utils.objects import stats
 
 
 def softmax_accuracy(probs, all_labels):
-    acc = (torch.argmax(probs) == all_labels).sum()
+    pred = probs > 0.5
+    acc = (pred == all_labels).sum()
     acc = torch.div(acc, len(all_labels) + 0.0)
     return acc
 